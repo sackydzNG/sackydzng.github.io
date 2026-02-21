8905aa67ced221425c8d8d6a260e3c2b5c97f439
SacKydzNG <sciencart12@gmail.com>
2026-02-21 16:08:26 +0530

    massive overhaul (not really)

---
+++ typingEffect.js
@@ -0,0 +1,17 @@
+function typeImage(i, t, ie, oe) {
+    input = document.getElementById(ie).innerHTML;
+    hello = input.split(" ");
+    document.getElementById(oe).innerHTML += hello[i];
+    setTimeout(function () {
+        ((i < hello.length - 1) ? typeImage(i + 1, t, ie, oe) : false);
+    }, t);
+}
+function appear(ie, t) {
+    const input = document.getElementById(ie);
+    setTimeout(() => {
+        input.style.display = 'block';
+    }, t)
+}
+typeImage(0, 1, "text", "screen");
+appear("box2", 4500);
+appear("box3", 5000);
